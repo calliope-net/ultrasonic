c2a60395450bd30b7667835e1518a3b837ce004b
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-14 12:23:35 +0200

    Aktualisiere pxt.json, main.blocks, ultrasonic.ts

--- ultrasonic.ts
+++ ultrasonic.ts
@@ -18,15 +18,49 @@ https://github.com/sparkfun/Zio-Qwiic-Ultrasonic-Distance-Sensor/blob/master/Ard
     const measure_command = 0x01
 
 
-    //% group="Ultraschall Sensor"
-    //% block="Entfernung in mm"
-    export function readDistance() {
-        if (pins.i2cWriteBuffer(i2cqwiicUltrasonic, Buffer.fromArray([0]), false) == 0) {
+    //% group="Qwiic Ultrasonic Distance Sensor"
+    //% block="beim Start I²C %newI2C"
+    //% newI2C.min=160 newI2C.max=175
+    export function beimStart(newI2C: number) {
+        if (newI2C >= 0xA0 && newI2C <= 0xAF) {
+            i2cqwiicUltrasonic = newI2C
+        }
+    }
+
+    //% group="Entfernung messen"
+    //% block="Entfernung in mm" weight=3
+    export function readDistancemm() {
+        if (pins.i2cWriteBuffer(i2cqwiicUltrasonic, Buffer.fromArray([measure_command]), false) == 0) {
             return pins.i2cReadBuffer(i2cqwiicUltrasonic, 2).getNumber(NumberFormat.UInt16BE, 0)
         } else {
             basic.showString(Buffer.fromArray([i2cqwiicUltrasonic]).toHex())
             return 0
         }
+    }
+
+    //% group="Entfernung messen"
+    //% block="Entfernung in cm" weight=2
+    export function readDistancecm() {
+        return Math.idiv(readDistancemm(), 10)
+    }
+
+
+    //% group="I²C"
+    //% block="I²C Adresse ändern %oldI2C neu %newI2C"
+    export function changeI2C(oldI2C: number, newI2C: number) {
+        //  Wire.beginTransmission(SLAVE_BROADCAST_ADDR); // transmit to device SLAVE_BROADCAST_ADDR
+        //  Wire.write(SLAVE_ADDR);              // Change the SLAVE_ADDR
+        //  Wire.endTransmission();    // stop transmitting
+        if (newI2C >= 0xA0 && newI2C <= 0xAF) {
+            if (pins.i2cWriteBuffer(oldI2C, Buffer.fromArray([newI2C]), false) == 0) {
+                i2cqwiicUltrasonic = newI2C
+                return true
+            } else {
+                basic.showString(Buffer.fromArray([oldI2C]).toHex())
+                return false
+            }
+        } else
+            return false
     }
 
 
